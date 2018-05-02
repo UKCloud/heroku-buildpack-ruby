fc732bd3964936694f14728ce579aaf2ccaa26a6
Jim Myhrberg <contact@jimeh.me>
2018-05-02 17:24:28 +0100

    Add "override vendor" concept allowing us to self-host binary archives
    Currently this is focussed on:
    
    - Ruby 2.1.6
    - Ruby 2.3.0
    - Ruby 2.3.4

--- lib/language_pack/fetcher.rb
+++ lib/language_pack/fetcher.rb
@@ -1,12 +1,23 @@
-require "yaml"
-require "language_pack/shell_helpers"
+require 'yaml'
+require 'language_pack/shell_helpers'
 
 module LanguagePack
   class Fetcher
     class FetchError < StandardError; end
 
     include ShellHelpers
-    CDN_YAML_FILE = File.expand_path("../../../config/cdn.yml", __FILE__)
+    CDN_YAML_FILE = File.expand_path('../../config/cdn.yml', __dir__)
+
+    OVERRIDE_VENDOR_URL = 'https://github.com/jimeh/' \
+                          'heroku-buildpack-ruby-binaries/' \
+                          'raw/master'.freeze
+    OVERRIDE_VENDOR_MAPPING = {
+      'bundler-1.15.1.tgz' => 'bundler-1.15.1.tgz',
+      'libyaml-0.1.7.tgz' => 'libyaml-0.1.7.tgz',
+      'ruby-2.1.6.tgz' => 'ruby-2.1.6.tgz',
+      'ruby-2.3.0.tgz' => 'ruby-2.3.0.tgz',
+      'ruby-2.3.4.tgz' => 'ruby-2.3.4.tgz'
+    }.freeze
 
     def initialize(host_url, stack = nil)
       @config   = load_config
@@ -15,13 +26,19 @@ module LanguagePack
     end
 
     def fetch(path)
+      path = path[1..-1] if path[0] == '/'
       curl = curl_command("-O #{@host_url.join(path)}")
       run!(curl, error_class: FetchError)
     end
 
     def fetch_untar(path, files_to_extract = nil)
+      path = path[1..-1] if path[0] == '/'
       curl = curl_command("#{@host_url.join(path)} -s -o")
-      run!("#{curl} - | tar zxf - #{files_to_extract}", error_class: FetchError)
+      STDOUT.puts 'DEBUG: TARGET: ' + run('echo $TARGET')
+      STDOUT.puts "DEBUG: #{curl}"
+      run! "#{curl} - | tar zxf - #{files_to_extract}",
+           error_class: FetchError,
+           max_attempts: 3
     end
 
     def fetch_bunzip2(path, files_to_extract = nil)
@@ -30,23 +47,41 @@ module LanguagePack
     end
 
     private
+
     def curl_command(command)
-      binary, *rest = command.split(" ")
+      binary, *rest = command.split(' ')
+
+      OVERRIDE_VENDOR_MAPPING.each do |k, v|
+        filename = File.basename(binary)
+        next unless filename.match(k)
+
+        binary = override_vendor_url.join(v)
+        command = ([binary] + rest).join(' ')
+        return "set -o pipefail; curl -L --fail --retry 5 --retry-delay 1 --connect-timeout #{curl_connect_timeout_in_seconds} --max-time #{curl_timeout_in_seconds} #{command}"
+      end
+
       buildcurl_mapping = {
-        "ruby" => /^ruby-(.+)$/,
-        "rubygem-bundler" => /^bundler-(.+)$/,
-        "libyaml" => /^libyaml-(.+)$/
+        'ruby' => /^ruby-(.+)$/,
+        'rubygem-bundler' => /^bundler-(.+)$/,
+        'libyaml' => /^libyaml-(.+)$/
       }
-      buildcurl_mapping.each do |k,v|
-        if File.basename(binary, ".tgz") =~ v
-          return "set -o pipefail; curl -L --get --fail --retry 3 #{buildcurl_url} -d recipe=#{k} -d version=#{$1} -d target=$TARGET #{rest.join(" ")}"
+      buildcurl_mapping.each do |k, v|
+        if File.basename(binary, '.tgz') =~ v
+          return "set -o pipefail; curl -L --get --fail --retry 3 #{buildcurl_url} -d recipe=#{k} -d version=#{Regexp.last_match(1)} -d target=$TARGET #{rest.join(' ')}"
         end
       end
+
       "set -o pipefail; curl -L --fail --retry 5 --retry-delay 1 --connect-timeout #{curl_connect_timeout_in_seconds} --max-time #{curl_timeout_in_seconds} #{command}"
     end
 
     def buildcurl_url
-      ENV['BUILDCURL_URL'] || "buildcurl.com"
+      ENV['BUILDCURL_URL'] || 'buildcurl.com'
+    end
+
+    def override_vendor_url
+      @override_vendor_url ||= Pathname.new(
+        ENV['OVERRIDE_VENDOR_URL'] || OVERRIDE_VENDOR_URL
+      )
     end
 
     def curl_timeout_in_seconds
